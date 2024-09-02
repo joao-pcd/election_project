a6111037ef1d31a989db8dc2f8069f15e955f0ad
joao-pcd <jclementinodias@gmail.com>
2024-09-02 07:15:31 -0300

    Election Management: Criação da classe de API

---
+++ election-management/src/main/java/api/CandidateApi.java
@@ -0,0 +1,28 @@
+package api;
+
+import domain.CandidateService;
+import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
+
+@ApplicationScoped
+public class CandidateApi {
+    private final CandidateService service;
+
+    public CandidateApi(CandidateService service) {
+        this.service = service;
+    }
+
+    public void create(api.dto.in.CreateCandidate dto) {
+        service.save(dto.toDomain());
+    }
+
+    public api.dto.out.Candidate update(String id, api.dto.in.UpdateCandidate dto) {
+        service.save(dto.toDomain(id));
+        return api.dto.out.Candidate.fromDomain(service.findById(id));
+    }
+
+    public List<api.dto.out.Candidate> list() {
+        return service.findAll().stream().map(api.dto.out.Candidate::fromDomain).toList();
+    }
+}
