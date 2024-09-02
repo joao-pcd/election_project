8d43e51987d7bec5aac53d42e395b66d3d19c9cb
joao-pcd <jclementinodias@gmail.com>
2024-09-02 07:12:41 -0300

    Election Management: Ajustes na classe domain

--- cicd-build.sh
+++ cicd-build.sh
@@ -10,8 +10,8 @@ APP_VERSION=$(./mvnw -q -Dexec.executable=echo -Dexec.args='${project.version}' 
 ./mvnw package
 ./mvnw versions:set -DnextSnapshot
 
-#git add pom.xml
-#git commit -m "cicd: bump version ${APP}:${APP_VERSION}"
+git add pom.xml
+git commit -m "cicd: bump version ${APP}:${APP_VERSION}"
 
 cd "$ROOT"
 TAG=$APP_VERSION docker compose build --no-cache "$APP"
